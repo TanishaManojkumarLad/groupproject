da66107929a72795bd0b658cf78f5e3d4d5e7662
Tanisha Manojkumar Lad <n01536175@humber.ca>
2023-04-09 18:59:20 -0400

    login and sign up page

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,8 @@
 export default function Home(){
-    return<div>Home page</div>
+    return<div>
+        <div style={{fontSize:"2 em", fontWeight:"bolder"}}>
+            Welcome to our Website!! If you are new to website please sign in or
+             you are current customer please Log in
+        </div>
+    </div>
 }
