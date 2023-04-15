519fe4a85ace21236a56c3b642f80a1434d9295b
xjocelyn123 <116309443+xjocelyn123@users.noreply.github.com>
2023-04-15 15:13:24 +0000

    updated home and home css, added food2.jpg

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,30 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
-export default function Home(){
-    return<div>
-        <div style={{fontSize:"2 em", fontWeight:"bolder"}}>
-            Welcome to our Website!! If you are new to website please sign in or
-             you are current customer please Log in
-        </div>
+const Home = () => {
+  return (
+    <div>
+      <img className="food" src={require("./food2.jpg")} alt="yummy" />
+      <div className="home-content">
+        <h1 className="heading-primary">
+          <span>welcome</span> <p className="white">to our</p> 𝓡𝓮𝓼𝓽𝓪𝓾𝓻𝓪𝓷𝓽!
+        </h1>
+
+        <p className="white">
+          The best chefs, the best dishes, the best restaurant in town
+        </p>
+        <p className="white">Checkout our menu:</p>
+        <Link to="/food" className="menu">
+          Menu
+        </Link>
+
+        <p className="white">
+          Call <span className="phone">416-237-3943</span>
+        </p>
+      </div>
     </div>
-}
+  );
+};
+
+export default Home;
